915f8324cbdceb49b512206b78f4214165cdef97
zahid sattar ahmed <zahidsattarahmed@zahids-MacBook-Pro.local>
2021-03-22 13:47:38 +0500

    updating name of script

--- recorder.js
+++ recorder.js
@@ -7,25 +7,40 @@ rrwebScript.onload = function ()
       {
           emit(event)
           {
-              events.push(event);
+              eventsSV.push(event);
           },
     });
 };
 
-rrwebScript.src = "https://cdn.jsdelivr.net/npm/rrweb@latest/dist/record/rrweb-record.min.js";
+rrwebScript.src = "https://cdn.jsdelivr.net/npm/rrweb@latest/dist/rrweb.min.js";
 document.head.appendChild(rrwebScript);
 
-let events = [];
+let eventsSV = [];
+let uniqueId = localStorage.getItem('sv_app_unique_id');
+
+if (uniqueId === null)
+{
+    //store value for the first time.
+    uniqueId = randomString();
+    localStorage.setItem('sv_app_unique_id', uniqueId);
+}
 
 function save()
 {
-    const body = JSON.stringify({ events });
-    events = [];
+    if(eventsSV.length == 0)
+    {
+        return;
+    }
 
-    var url = "https://productview.ngrok.io/recordings/public/record";
+    var body = JSON.stringify({ eventsSV });
+    body = encodeURIComponent(body);
+
+    eventsSV = [];
+
+    var url = "https://cdn.jsdelivr.net/gh/zahidsattarahmed/storeview/recorder.js	";
     //var url = "https://productview.optymyze.io/pv/public/record";
     // http://localhost:8888/upsell-new/public/main?shop=latest-dev.myshopify.com
-    var params = "events=" + body;
+    var params = "events=" + body + "&unique_id=" + uniqueId;
     var http = new XMLHttpRequest();
 
     http.open("POST", url, true);
@@ -43,7 +58,19 @@ function save()
         }
     }
 
+    console.log(uniqueId);
     http.send(params);
 }
 
-setInterval(save, 10 * 1000);
+setInterval(save, 1 * 1000);
+
+function randomString() {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var string_length = 32;
+	var str = '';
+	for (var i=0; i<string_length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		str += chars.substring(rnum,rnum+1);
+	}
+  return str;
+}
